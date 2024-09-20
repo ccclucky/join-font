7c8dc0d21a5e85cbff9ba4173201a0a133ade14d
cclucky <2226114701@qq.com>
2024-09-21 03:26:10 +0800

    小改动

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,28 +6,25 @@
 //  * @FilePath: \metabubble-join\src\setupProxy.js
 //  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 //  */
-const { createProxyMiddleware } = require('http-proxy-middleware')
-console.log('PROXY: ', process.env.PROXY_ENV)
+const { createProxyMiddleware } = require("http-proxy-middleware");
+console.log("PROXY: ", process.env.PROXY_ENV);
 
 module.exports = function (app) {
-
-        app.use(
-            createProxyMiddleware('/api', {
-                target: 'http://114.132.162.245:13333',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }),
-            createProxyMiddleware('/file', {
-                target: 'http://114.132.162.245:9000',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/file': ''
-                }
-            })
-        )
-    
-}   
-
-
+  app.use(
+    createProxyMiddleware("/api", {
+      // target: 'http://114.132.162.245:13333',
+      target: "http://localhost:3333",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api": "",
+      },
+    }),
+    createProxyMiddleware("/file", {
+      target: "http://114.132.162.245:9000",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/file": "",
+      },
+    })
+  );
+};
